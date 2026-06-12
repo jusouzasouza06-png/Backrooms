a58f05575efc45289e9d6b08ac23b3c5fd2d2d23
jusouzasouza06-png <jusouza.souza06@gmail.com>
2026-06-12 18:27:41 -0300

    Criar controls.js

---
+++ control/controls/controls.js
@@ -0,0 +1,9 @@
+export const keys = {};
+
+document.addEventListener("keydown", e => {
+    keys[e.key.toLowerCase()] = true;
+});
+
+document.addEventListener("keyup", e => {
+    keys[e.key.toLowerCase()] = false;
+});
