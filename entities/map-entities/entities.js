938cbd82add4ecdb5ceede39accb9d2308129303
jusouzasouza06-png <jusouza.souza06@gmail.com>
2026-06-12 22:05:44 -0300

    Atualizar o entities.js

--- entities/map-entities/entities.js
+++ entities/map-entities/entities.js
@@ -1,1 +1,24 @@
-u
+import * as THREE from 'https://unpkg.com/three@0.165.0/build/three.module.js';
+
+export class Entity {
+
+    constructor(scene){
+
+        this.mesh = new THREE.Mesh(
+            new THREE.BoxGeometry(1,2,1),
+            new THREE.MeshLambertMaterial({color:0x000000})
+        );
+
+        this.mesh.position.set(10,1,10);
+        scene.add(this.mesh);
+    }
+
+    update(player){
+
+        this.mesh.position.x +=
+            (player.position.x - this.mesh.position.x) * 0.002;
+
+        this.mesh.position.z +=
+            (player.position.z - this.mesh.position.z) * 0.002;
+    }
+}
